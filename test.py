86bc64e45b5ada86619f1129698bca83ae2ca8ec
szyszy315 <zeyusong@bu.edu>
2020-01-27 21:18:54 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,22 @@
-import inttoroma.py
+import inttoroma
 
 def test():
+  if inttoroma.intToRoman(1) != 'I':
+    return False
+  if inttoroma.intToRoman(4) != 'IV':
+    return False
+  if inttoroma.intToRoman(10) != 'X':
+    return False
+  if inttoroma.intToRoman(40) != 'XL':
+    return False
+  if inttoroma.intToRoman(400) != 'CD':
+    return False
+  if inttoroma.intToRoman(500) != 'D':
+    return False
+  if inttoroma.intToRoman(900) != 'CM':
+    return False
+  if inttoroma.intToRoman(3000) != 'MMM':
+    return False
+  if inttoroma.intToRoman(3111) != 'MMMCXI':
+    return False
+  return True
