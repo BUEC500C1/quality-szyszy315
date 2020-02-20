1a9b4ffd2073ddc68a16f8f18eb28b102ece556e
szyszy315 <zeyusong@bu.edu>
2020-02-19 19:03:33 -0500

    Update inttoroma.py

--- inttoroma.py
+++ inttoroma.py
@@ -1,4 +1,7 @@
 def intToRoman(num):
+    if (type(num) != int):
+      print ("not supported input")
+      return
     count = 0
     o =[]
     d = {1000: 'M', 900: 'CM', 500: 'D', 400: 'CD', 100: 'C', 90: 'XC', 50: 'L', 40: 'XL', 10: 'X', 9: 'IX', 5: 'V', 4: 'IV', 1: 'I'}
@@ -6,4 +9,5 @@ def intToRoman(num):
         while num >= n:
             num = num - n
             o.append(d[n])
+    print("not supported input")
     return (''.join(o))
