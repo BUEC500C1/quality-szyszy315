c4e91b0a8526d522236e4e113645771840177305
szyszy315 <zeyusong@bu.edu>
2020-02-26 17:44:03 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,22 +1,15 @@
 import inttoroma
+import pytest
 
 def test():
-  if inttoroma.intToRoman(1) != 'I':
-    return False
-  if inttoroma.intToRoman(4) != 'IV':
-    return False
-  if inttoroma.intToRoman(10) != 'X':
-    return False
-  if inttoroma.intToRoman(40) != 'XL':
-    return False
-  if inttoroma.intToRoman(400) != 'CD':
-    return False
-  if inttoroma.intToRoman(500) != 'D':
-    return False
-  if inttoroma.intToRoman(900) != 'CM':
-    return False
-  if inttoroma.intToRoman(3000) != 'MMM':
-    return False
-  if inttoroma.intToRoman(3111) != 'MMMCXI':
-    return False
-  return True
+  assert inttoroma.intToRoman(1) == 'I'
+  assert inttoroma.intToRoman(4) == 'IV'
+  assert inttoroma.intToRoman(10) == 'X'
+  assert inttoroma.intToRoman(40) == 'XL'
+  assert inttoroma.intToRoman(400) == 'CD'
+  assert inttoroma.intToRoman(500) == 'D'
+  assert inttoroma.intToRoman(900) == 'CM'
+  assert inttoroma.intToRoman(3000) == 'MMM'
+  assert inttoroma.intToRoman(3111) == 'MMMCXI'
+  assert inttoroma.intToRoman("abc") == "not supported input"
+  assert inttoroma.intToRoman(2.2) == "not supported input"
